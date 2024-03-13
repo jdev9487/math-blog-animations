0407a56d4e366a4d74c626e61fda1dae1e5213d4
John Gould <johngould9487@protonmail.com>
2024-03-13 20:13:22 +0100

    install latex on runner

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -1,3 +1,4 @@
 sudo apt update
-sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
+sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev texlive-latex-recommended
+ffmpeg
 pip3 install manim
