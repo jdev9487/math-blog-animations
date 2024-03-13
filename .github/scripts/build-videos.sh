28a33b959e8aec4c0a01d110612b8ef3361bd124
John Gould <johngould9487@protonmail.com>
2024-03-13 20:49:27 +0100

    low quality videos

--- .github/scripts/build-videos.sh
+++ .github/scripts/build-videos.sh
@@ -3,7 +3,7 @@ for path in *.py
 do
     fullfilename=$(basename -- "$path")
     filename="${fullfilename%.*}"
-    manim -iqh $path Source
+    manim -iql $path Source
     mv media/videos/$filename/1080p60/*.gif output/$filename.gif
 done
 rm -rf media
