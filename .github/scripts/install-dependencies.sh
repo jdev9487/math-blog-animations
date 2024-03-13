4f39b6e325e0a610cbf09159dd9ccc95e9579435
John Gould <johngould9487@protonmail.com>
2024-03-13 20:20:50 +0100

    tex extra installed

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev texlive-latex-recommended ffmpeg
+sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev texlive texlive-latex-extra ffmpeg
 pip3 install manim
