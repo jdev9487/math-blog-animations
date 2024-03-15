9ccebdc5ddd3e2cf9cd904b2bda88d2eb944ebde
John Gould <johngould9487@protonmail.com>
2024-03-15 09:59:48 +0100

    cloudconvert mp4s

--- .github/scripts/build-videos.sh
+++ .github/scripts/build-videos.sh
@@ -1,9 +1,12 @@
+export CLOUDCONVERT_API_KEY=$1
 mkdir output
 for path in *.py
 do
     fullfilename=$(basename -- "$path")
     filename="${fullfilename%.*}"
     manim -qh $path Source
-    mv media/videos/$filename/480p15/*.gif output/$filename.gif
+    mv media/videos/$filename/1080p60/*.mp4 output/$filename.mp4
 done
 rm -rf media
+
+node_modules/cloudconvert-cli/lib/cli.js convert -f gif output/*.mp4
--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -1,5 +1,4 @@
 npm install cloudconvert-cli
-node_modules/cloudconvert-cli/lib/cli.js --version
 sudo apt update
 sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev texlive texlive-latex-extra ffmpeg
 pip3 install manim
