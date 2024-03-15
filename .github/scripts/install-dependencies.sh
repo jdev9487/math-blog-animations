b5a15c02c1d730c238c23f924f882556e3b5d767
John Gould <johngould9487@protonmail.com>
2024-03-15 09:33:37 +0100

    cloudconvert cli

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -1,3 +1,5 @@
+npm install -g cloudconvert-cli
+couldconvert --version
 sudo apt update
 sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev texlive texlive-latex-extra ffmpeg
 pip3 install manim
