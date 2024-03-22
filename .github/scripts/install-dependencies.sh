243e957b1b5cdc3fb199a5536f45e7b86efd2f26
John Gould <johngould9487@protonmail.com>
2024-03-22 22:19:28 +0100

    install yq on runner

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -1,3 +1,5 @@
 sudo apt update
 sudo apt install python3-pip build-essential python3-dev libcairo2-dev libpango1.0-dev texlive texlive-latex-extra ffmpeg
 pip3 install manim
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+sudo chmod a+x /usr/local/bin/yq
