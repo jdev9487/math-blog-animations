31e3ffe5853c59ffcb027cdd9779c8727a85918d
John Gould <johngould9487@protonmail.com>
2025-07-10 20:46:43 +0100

    removed thumbnails

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def animation(slug):
     filename = f'output/{slug}.mp4'
     return send_file(filename, mimetype='image/mp4')
 
-@app.get("/thumbnails/<slug>")
-def thumbnail(slug):
-    filename = f'output/{slug}.png'
-    return send_file(filename, mimetype='image/png')
-
 if __name__ == "__main__":
     app.run()
--- scripts/build-videos.sh
+++ scripts/build-videos.sh
@@ -21,8 +21,5 @@ do
     directoryName=$(basename -- "$path")
     manim -q$1 $pythonSource Source
     mv media/videos/source/$videoDir/Source.mp4 output/$directoryName.mp4
-
-    timestamp=$(yq '.thumbnail' "./$settingSource")
-    ffmpeg -i output/$directoryName.mp4 -ss $timestamp -vframes 1 output/$directoryName.png
 done
 rm -rf media
